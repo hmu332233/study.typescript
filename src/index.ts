84f2602c3d667d04f041f7de8eb6d9204b2f8bd4
minung.han <hmu332233@gmail.com>
2020-10-18 23:21:18 +0900

    6. Classes on Typescript part One

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
-interface Human {
-  name: String,
-  age: Number,
-  gender: String
+class Human {
+  public name: String;
+  public age: Number;
+  public gender: String;
+
+  constructor(name: String, age: Number, gender?: String) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 };
 
-const person = {
-  name: 'minung',
-  age: 28,
-  gender: 'male',
-};
+const minung = new Human('minung', 28, 'male');
 
 const sayHi = (person: Human): String => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 }
 
-console.log(sayHi(person));
+console.log(sayHi(minung));
 
 export {};
