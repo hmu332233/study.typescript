670f9a75a51cdfe73774d332da4e5b7251235639
minung.han <hmu332233@gmail.com>
2020-12-27 01:33:55 +0900

    learn 3

---
+++ learn/3/index.ts
@@ -0,0 +1,21 @@
+// 문자열
+const str: string = 'hello';
+
+// 숫자
+const num: number = 10;
+
+// 배열
+const arr: Array<string> = [];
+const items: string[] = [];
+
+// 튜플 - 타입과 위치까지 지정
+const address: [string, number] = ['테스트', 111];
+
+// 객체
+const obj: object = {};
+
+// 타입 객체
+const person: { age: number; name: string } = { age: 30, name: 'Test' };
+
+// 진위값
+let isLogin: boolean = false;
