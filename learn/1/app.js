6a042318953282e246d06e8c1cf15075ea11d0b4
minung.han <hmu332233@gmail.com>
2020-12-27 00:31:08 +0900

    learn jsdoc 사용

--- learn/1/app.js
+++ learn/1/app.js
@@ -9,6 +9,26 @@ var address = document.querySelector('#address');
 // user data
 var user = {};
 
+/**
+ * @typedef {object} Address
+ * @property {string} street
+ * @property {string} city
+ */
+
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {Address} address
+ */ 
+
+/**
+ * @returns {Promise<User>}
+ */
+function fetchUser() {
+  return axios.get(url);
+}
+
 function startApp() {
   axios
     .get(url)
