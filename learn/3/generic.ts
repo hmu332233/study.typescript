26b7521808110aa94dd36765d334221db7e98e47
minung.han <hmu332233@gmail.com>
2020-12-30 16:50:58 +0900

    learn generic extends keyof

--- learn/3/generic.ts
+++ learn/3/generic.ts
@@ -20,57 +20,65 @@ logText<string>('test').split(''); // string임이 명확하므로 관련 함수
 logText<number>(10).toFixed(2) // number임이 명확하므로 관련 함수도 문제 없이 사용 가능
 
 
-
-function getNumber(value: number) {
-  return value;
-}
-
-function getArray(value: string[]) {
-  return value;
-}
-
-// 제네릭 기본 문법 - 함수
-function getValue<T>(value: T): T {
-  return value;
-}
-getValue('hi').toLocaleUpperCase();
-getValue(100).toLocaleString();
-
 // 제네릭 기본 문법 - 인터페이스
-interface Developer<T> {
-  name: string;
-  age: T;
-}
-const tony: Developer<number> = { name: 'tony', age: 100 };
-
-// 제네릭 타입 제한 - 구체적인 타입
-function getNumberAndArray<T>(value: T): T {
-  value.length; // X
-  return value;
+interface DropdownItem<T> {
+  value: T;
+  selected: boolean;
 }
 
-function getNumberAndArray<T>(value: T[]): T[] {
-  value.length; // O
-  return value;
+const stringItems: DropdownItem<string>[] = [
+  { value: 'test', selected: true }
+];
+
+// 제네릭 타입 제한
+// 제네릭 타입 제한 - 정의된 타입 이용하기
+function logTextLength<T>(text: T): T {
+  // 어떤 타입이 들어올지 알 수 없으므로 length라는 함수가 있을지 보장이 안됨
+  return text.length;
 }
 
-// 제네릭 타입 제한 - keyof
-interface ShoppingItems {
+interface LengthType {
+  length: number;
+}
+
+// LengthType에서 정의된 문법을 가지고 있는 타입만을 사용하겠다고 제한을 걸 수 있도록 해주는게 extends
+
+// length 속성을 가지고 있을 경우에만 제너릭 타입으로 사용할 수 있다
+function logTextLength2<T extends LengthType>(text: T): number {
+  return text.length;
+}
+
+logTextLength2('test');
+logTextLength2({ length: 1, value: 2 });
+logTextLength2(10)
+
+
+// 제네릭 타입 제한 - keyof 이용하기
+// 인터페이스에 정의된 속성 중에 하나만 받겠다고 제한을 걸 수 있도록 해주는게 keyof
+interface ShoppingItem {
   name: string;
   price: number;
-  address: string;
   stock: number;
 }
-function getAllowedOptions<T extends keyof ShoppingItems>(option: T): T {
-  if (option === 'name' || option === 'address') {
-    console.log('option type is string');
-    return option;
-  }
-  if (option === 'price' || option === 'stock') {
-    console.log('option type is number');
-    return option;
-  }
+
+// ShoppingItem에 정의된 key값들만 제너릭으로 사용 가능하다는 의미
+// 예시 기준으로 name, price, stock만 넣을 수 있다는 의미이다.
+function getShoppingItemOption<T extends keyof ShoppingItem>(itemOption: T): T  {
+  return itemOption;
 }
-getAllowedOptions('nothing');
-// const a = getAllowedOptions('name');
-// a.toUpperCase(); // Name
+
+getShoppingItemOption('name');
+
+// function getAllowedOptions<T extends keyof ShoppingItems>(option: T): T {
+//   if (option === 'name' || option === 'address') {
+//     console.log('option type is string');
+//     return option;
+//   }
+//   if (option === 'price' || option === 'stock') {
+//     console.log('option type is number');
+//     return option;
+//   }
+// }
+// getAllowedOptions('nothing');
+// // const a = getAllowedOptions('name');
+// // a.toUpperCase(); // Name
